6c575d3dd18cb41a323bd17a94095a37ecd439ff
abdatorey <abdatorey@gmail.com>
2025-06-30 22:04:06 +0100

    commented a third line

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -70,7 +70,7 @@ variable "data_bucket_location" {
   default     = "EU-WEST1"
 }
 
-# --- BigQuery Module Variables biqury---
+# --- BigQuery Module Variables biquerery---
 variable "bq_dataset_id" {
   description = "The ID for the BigQuery dataset."
   type        = string
